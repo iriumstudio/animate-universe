03fd97d016ea6c7d3f7027df42d86e277d0b7fd7
iriumstudio <modrew9@gmail.com>
2026-03-28 22:08:43 +0700

    add google tag

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,22 @@
-import "./globals.css";
-
-export const metadata = {
-  title: "AniMate Universe",
-  description: "Landing page for AniMate Universe",
-};
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="th">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-1766774864"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-1766774864');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
